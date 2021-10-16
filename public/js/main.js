7e791f5f8320f918b8aade24f4f16ed511a06440
Negena <justnegena@gmail.com>
2021-10-16 22:18:27 +0500

    push chat

---
+++ public/js/main.js
@@ -0,0 +1,62 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+const nameOfUse = document.getElementById("names-of-use");
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+socket.emit('joinRoom', {username, room});
+
+socket.on("roomUsers", ({ room , users, username  })=> {
+  outputRoomName(room);
+  outputUsers(users);
+  names(username);
+});
+
+socket.on("message", message => {
+  //console.log(message);
+  outputMessage(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//message submit
+chatForm.addEventListener("submit", (e)=> {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  socket.emit("chatMessage", msg);
+
+    e.target.elements.msg.value = ' ';
+    e.target.elements.msg.focus();
+});
+
+function outputMessage(message){
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
+
+function outputRoomName(room){
+ roomName.innerText = room;
+}
+
+function outputUsers(users){
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join(" ")}
+  `;
+}
+
+function names(user, username){
+    //const user = users.find(user => user.username === user)
+   nameOfUse.innerText = user.username
+};
